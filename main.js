264ca20b896d3ad599789fdf83138de9489d7781
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-19 21:46:49 -0500

    getting there

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-console.log("PERSONAL BIO");	
+import 'bootstrap';
+
 
 const projects = [	
-    {	
-        title: 'Pet Adoption',	
-        screenshot: 'https://i.ibb.co/ysVkhXK/Pet-Adoption-SS.jpg',	
-        description: 'A real cute pet adoption website for the lonley and longing.',	
-        techUsed: 'HTML, CSS, Flexbox',	
-        available: true,	
-        url: 'https://github.com/AaronLain/pet-adoption',	
-        githubUrl: 'https://github.com/AaronLain/pet-adoption'	
-    },	
-    {
-        title: 'Bear Watcher',	
-        screenshot: 'https://i.ibb.co/f146635/Screen-Shot-2020-03-02-at-18-11-00.png',	
-        description: 'A website for watching adorable bears catch fish.',	
-        techUsed: 'HTML, CSS, Bootstrap',	
-        available: true,	
-        url: 'https://github.com/AaronLain/bear-watcher',	
-        githubUrl: 'https://github.com/AaronLain/bear-watcher'
-    },
-    {
-        title: 'Sorting Hat',	
-        screenshot: 'https://i.ibb.co/tpjymVr/Screen-Shot-2020-02-18-at-10-14-35.png',	
-        description: 'A sorting portal for students entering Hogwarts',	
-        techUsed: 'HTML, CSS, Bootstrap',	
-        available: true,	
-        url: 'https://github.com/AaronLain/sorting-hat',	
-        githubUrl: 'https://github.com/AaronLain/sorting-hat'
-    }
-]	
+{
+    title: 'Pet Adoption',
+    screenshot: 'https://i.ibb.co/ysVkhXK/Pet-Adoption-SS.jpg',
+    description: 'A real cute pet adoption website for the lonley and longing.',
+    techUsed: 'HTML, CSS, Flexbox',
+    available: true,
+    url: 'https://github.com/AaronLain/pet-adoption',
+    githubUrl: 'https://github.com/AaronLain/pet-adoption',
+},
+{
+    title: 'Bear Watcher',
+    screenshot: 'https://i.ibb.co/f146635/Screen-Shot-2020-03-02-at-18-11-00.png',
+    description: 'A website for watching adorable bears catch fish.',
+    techUsed: 'HTML, CSS, Bootstrap',
+    available: true,
+    url: 'https://github.com/AaronLain/bear-watcher',
+    githubUrl: 'https://github.com/AaronLain/bear-watcher',
+},
+{
+    title: 'Sorting Hat',
+    screenshot: 'https://i.ibb.co/tpjymVr/Screen-Shot-2020-02-18-at-10-14-35.png',
+    description: 'A sorting portal for students entering Hogwarts',
+    techUsed: 'HTML, CSS, Bootstrap',
+    available: true,
+    url: 'https://github.com/AaronLain/sorting-hat',
+    githubUrl: 'https://github.com/AaronLain/sorting-hat',
+}
+]
 
 const printToDom = (divId, textToPrint) =>{	
     const selectedDiv = document.getElementById(divId);	
