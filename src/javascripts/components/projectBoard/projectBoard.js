cf229c9095530177f5d99f5ea2ef6ddafd5dcf1c
Aaron Lain <aaron.j.lain@gmail.com>
2020-04-05 20:54:43 -0500

    project board prints, data in place, api works

---
+++ src/javascripts/components/projectBoard/projectBoard.js
@@ -0,0 +1,20 @@
+import projectsData from '../../helpers/data/projectsData';
+import utils from '../../helpers/utils';
+import projectComponent from '../projectCards/projectCards';
+
+const buildProjectBoard = () => {
+  projectsData.getProjects()
+    .then((projects) => {
+      let domString = '';
+      console.error(projects);
+      // domString += '<div class="d-flex flex-wrap">';
+      projects.forEach((project) => {
+        domString += projectComponent.createProjectCards(project);
+      });
+      // domString += '</div>';
+      utils.printToDom('card-container', domString);
+    })
+    .catch((err) => console.error('get projects broke', err));
+};
+
+export default { buildProjectBoard };
